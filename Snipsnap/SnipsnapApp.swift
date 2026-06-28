0cc04b9eef6c10939a96bbb8c038b0ee0cfc8770
Ethan Watson <ethanwatsonj88@gmail.com>
2026-06-27 20:08:19 -0400

    Initial Commit

---
+++ Snipsnap/SnipsnapApp.swift
@@ -0,0 +1,32 @@
+//
+//  SnipsnapApp.swift
+//  Snipsnap
+//
+//  Created by Ethan Watson on 6/27/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SnipsnapApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
